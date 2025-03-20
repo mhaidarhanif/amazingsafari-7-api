c7485b94f3ed51ee05d6ef09afe5df72a50189d8
M Haidar Hanif <git@mhaidarhanif.com>
2025-03-20 22:09:21 +0700

    feat: setup prisma, migration, get products

---
+++ prisma/data/products.ts
@@ -0,0 +1,16 @@
+export const dataProducts = [
+  {
+    name: "Panda Cushion",
+    slug: "panda-cushion",
+    price: 10,
+    imageUrl:
+      "https://ucarecdn.com/89ffcef2-5e5a-422e-ab70-e9a569e59dc6/-/preview/500x500/",
+  },
+  {
+    name: "Bear Pillow",
+    slug: "bear-pillow",
+    price: 15,
+    imageUrl:
+      "https://ucarecdn.com/89ffcef2-5e5a-422e-ab70-e9a569e59dc6/-/preview/500x500/",
+  },
+];
